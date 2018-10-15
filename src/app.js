3e7b85edaf220f8cf74e0f647a6bfd254f925b5b
krishnaprofessional <krishna.ece1@gmail.com>
2018-10-15 11:21:03 +0200

    First Commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import DecisionApp from './components/DecisionApp';
+import 'normalize.css/normalize.css';
+  
+  ReactDOM.render(<DecisionApp />, document.getElementById('app'));
+     
